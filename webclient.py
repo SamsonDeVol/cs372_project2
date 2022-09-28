a45da3426a3cd277a981f15395cdf751811aa4e9
SamsonDeVol <devols@oregonstate.edu>
2022-09-28 09:23:23 -0700

    append recieve bits for webclient

--- webclient.py
+++ webclient.py
@@ -13,20 +13,22 @@ else:
 # ask OS for a socket
 s = socket.socket()
 
-# perform DNS lookup
-#connect the socket
+# connect the socket
 s.connect((server_name,port_number))
 
 # send data
 s.sendall("GET / HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(server_name).encode())
 
-# recieve data
+
+full_message = b''
+# recieve data 4096 bytes at a time and append
 while 1: 
-  d = s.recv(4096)  # Receive up to 4096 bytes
-  print(d)
+  d = s.recv(4096)
   if len(d)==0:
     break
-
+  else: 
+    full_message += d
+  
 # close
 s.close()
-print(d)
+print(full_message)
